f9200af5b815c4a7d3fbad89ee46cf0243fba9cf
anagha-kj <159282427+anagha-kj@users.noreply.github.com>
2024-04-07 20:47:28 -0700

    feat: Play word audio

---
+++ MyDictionary/View/PlayAudio.swift
@@ -0,0 +1,18 @@
+//
+//  PlayAudio.swift
+//  MyDictionary
+//
+//  Created by Anagha K J on 07/04/24.
+//
+
+import SwiftUI
+
+struct PlayAudio: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayAudio()
+}
