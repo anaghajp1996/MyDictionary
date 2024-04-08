87f26e7bec697ce13f37efe18445f8a160747dbd
anagha-kj <159282427+anagha-kj@users.noreply.github.com>
2024-04-07 18:09:49 -0700

    initial commit

---
+++ MyDictionary/MyDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDictionaryApp.swift
+//  MyDictionary
+//
+//  Created by Anagha K J on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MyDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
